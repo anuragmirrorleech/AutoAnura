dc17e77d2487464f78a53edf82c6fcd5734c2776
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2022-10-28 15:36:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AM-ROBOTS/AutoAnurag.git /AutoAnurag
+  git clone https://github.com/anuragmirrorleech/AutoAnura /AutoAnura
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoAnurag
+  git clone $UPSTREAM_REPO /AutoAnura
 fi
-cd /AutoAnurag
+cd /AutoAnura
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
