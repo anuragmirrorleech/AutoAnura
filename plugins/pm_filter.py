6d76adc704fbda72205a5b3a4f117f497a400f37
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2022-11-24 00:51:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -835,15 +835,18 @@ async def manual_filters(client, message, text=False):
             if reply_text:
                 reply_text = reply_text.replace("\\n", "\n").replace("\\t", "\t")
 
-
             if btn is not None:
                 try:
                     if fileid == "None":
                         if btn == "[]":
-                        hlo = await client.send_message(group_id,reply_text,disable_web_page_preview=True,reply_to_message_id=reply_id)  
-                       if SELF_DELETE:
-                          await asyncio.sleep(SELF_DELETE_SECONDS)
-                          await hlo.delete()
+                             hlo = await client.send_message(
+                                group_id, 
+                                reply_text, 
+                                disable_web_page_preview=True,
+                                reply_to_message_id=reply_id)
+                             if SELF_DELETE:
+                                await asyncio.sleep(SELF_DELETE_SECONDS)
+                                await hlo.delete()
                         else:
                             button = eval(btn)
                       hmm = await client.send_message(
