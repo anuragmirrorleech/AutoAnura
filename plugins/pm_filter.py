00b8b6a908ca299d7b54cb58beff6bd92cc3d4f0
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2022-11-24 00:44:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -844,7 +844,8 @@ async def manual_filters(client, message, text=False):
                                 group_id, 
                                 reply_text, 
                                 disable_web_page_preview=True,
-                                reply_to_message_id=reply_id)
+                                reply_to_message_id=reply_id
+                            )
                       if SELF_DELETE:
                          await asyncio.sleep(SELF_DELETE_SECONDS)
                          await hmm.delete()
